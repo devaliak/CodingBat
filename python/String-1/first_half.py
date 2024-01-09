67b2aaa3b1a72a55de46d0c9856e830f49a47bc3
Ali Ak <aliakbartin@gmail.com>
2024-01-09 17:43:37 -0500

    added String-1

---
+++ python/String-1/first_half.py
@@ -0,0 +1,9 @@
+"""
+Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+"""
+
+
+def first_half(str):
+    half = len(str) / 2
+    return str[:half]
---
+++ python/String-1/make_abba.py
@@ -0,0 +1,8 @@
+"""
+Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+
+"""
+
+
+def make_abba(a, b):
+    return '{0}{1}{1}{0}'.format(a, b, b, a)
